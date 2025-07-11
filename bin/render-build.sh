040b8a10ae0d4e9cc227fab77b23ec696e57f518
Lily-Gold <lily.olive.yt@gmail.com>
2025-07-11 22:59:33 +0900

    fix

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,10 @@
+#!/usr/bin/env bash
 set -o errexit
 
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
+
+# 👇 ここを一時的に追加
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:seed:replant
