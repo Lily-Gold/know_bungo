ccc081c4c6dc68d8a5d097cf07ac449da8f557d0
Lily-Gold <lily.olive.yt@gmail.com>
2025-07-11 21:15:16 +0900

    Add render.yaml for Render deployment config

--- app/controllers/bungaos_controller.rb
+++ app/controllers/bungaos_controller.rb
@@ -2,6 +2,11 @@ class BungaosController < ApplicationController
   def random
     @bungo = Bungo.order("RANDOM()").first
 
+    unless @bungo
+      flash[:alert] = "文豪データが見つかりませんでした。"
+      redirect_to root_path and return
+    end
+
     respond_to do |format|
       format.html
       format.turbo_stream
