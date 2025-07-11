eedf56fea8593c2b1115a71bf25a785599f63a11
Lily-Gold <lily.olive.yt@gmail.com>
2025-07-11 21:26:01 +0900

    fix

--- app/controllers/bungaos_controller.rb
+++ app/controllers/bungaos_controller.rb
@@ -1,11 +1,12 @@
 class BungaosController < ApplicationController
   def random
     @bungo = Bungo.order("RANDOM()").first
+    Rails.logger.info "Random Bungo: #{@bungo.inspect}"
 
-    unless @bungo
-      flash[:alert] = "文豪データが見つかりませんでした。"
-      redirect_to root_path and return
-    end
+    # unless @bungo
+    #   flash[:alert] = "文豪データが見つかりませんでした。"
+    #   redirect_to root_path and return
+    # end
 
     respond_to do |format|
       format.html
