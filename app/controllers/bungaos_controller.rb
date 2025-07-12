81507f6c51fa55d0eb1f58e1a18544495307af9d
Lily-Gold <lily.olive.yt@gmail.com>
2025-07-12 17:13:43 +0900

    add:X共有文面変更

--- app/controllers/bungaos_controller.rb
+++ app/controllers/bungaos_controller.rb
@@ -3,10 +3,9 @@ class BungaosController < ApplicationController
     @bungo = Bungo.order("RANDOM()").first
     Rails.logger.info "Random Bungo: #{@bungo.inspect}"
 
-    # unless @bungo
-    #   flash[:alert] = "文豪データが見つかりませんでした。"
-    #   redirect_to root_path and return
-    # end
+    if @bungo.present?
+      @tweet_text = "#{@bungo.name}について知ったよ！これであなたも文豪マスター！"
+    end
 
     respond_to do |format|
       format.html
